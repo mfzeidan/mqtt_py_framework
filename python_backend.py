d1b5e86cb25c9151b68c8031b2c925e6568a8f17
Mark Zeidan <mfzeidan@gmail.com>
2016-10-12 18:18:30 -0400

    Create python_backend.py
    this is where we'll build the nuts and bolts of python package

---
+++ python_backend.py
@@ -0,0 +1,13 @@
+def helloworld():
+   print "hello"
+
+
+def goodbye():
+   print "goodbye"
+
+
+def getting_started(server,topic,message):
+    print "You're now connected to " + str(server)
+    print "You're now publishing your messages to " + str(topic)
+    print "You just sent " + str(message) + "to the following topic " + str(topic)
+   
