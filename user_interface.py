9cfc7fe44bffab94ecdb5869beeced61bc250664
Mark Zeidan <mfzeidan@gmail.com>
2016-10-12 19:05:47 -0400

    trying to get this file to inherit a class built into the other module

--- user_interface.py
+++ user_interface.py
@@ -1,7 +1,23 @@
 import mqtt_framework as mqtt
 
-server = "192.168.0.1"
+import socket
+import fcntl
+import struct
+
+port = 'eth0'
+
+server = mqtt.get_ip_address(port)
+
+
 topic = "mytopic"
 message = "this is my first message"
 
-mqtt.getting_started(server,topic,message)
+#mqtt.getting_started(server,topic,message)
+
+#mqtt.receive_messages(server, topic,message)
+
+#mqtt.publish_messages(server,topic,message)
+
+damsg = mqtt.test()
+
+damsg.publish_msg()
