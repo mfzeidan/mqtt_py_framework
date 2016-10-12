2f2d8b89d01fefb21ad371b4b3017fa7d7a9425f
Mark Zeidan <mfzeidan@gmail.com>
2016-10-12 18:30:52 -0400

    Update python_backend.py

--- python_backend.py
+++ python_backend.py
@@ -1,13 +1,18 @@
-def helloworld():
-   print "hello"
+import socket
+import fcntl
+import struct	
 
-
-def goodbye():
-   print "goodbye"
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
 
 
 def getting_started(server,topic,message):
     print "You're now connected to " + str(server)
     print "You're now publishing your messages to " + str(topic)
-    print "You just sent " + str(message) + "to the following topic " + str(topic)
+    print "You just sent " + str(message) + " to the following topic " + str(topic)
    
